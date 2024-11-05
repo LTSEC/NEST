59b7d661a684b16755aa32c2def006aff10b9be9
Aidan Feess <aidanfeess@gmail.com>
2024-11-04 22:12:16 -0600

    test update for new website

--- web/handlers.go
+++ web/handlers.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"context"
 	"encoding/json"
 	"errors"
 	"strconv"
@@ -21,24 +22,29 @@ var (
 
 // Handler for serving the table page
 func TableHandler(c echo.Context) error {
+	// Fetch teams and services from the score_holder or other sources
 	allTeams := score_holder.GetMap()
 	if len(allTeams) > 0 {
+		// Preparing teams and services data
 		teams := make([]string, len(allTeams))
 		for i := range allTeams {
 			teams[i] = "Team " + strconv.Itoa(i)
 		}
 		services := []string{"ftp", "http", "ssh"}
+
+		// Render the Table component with teams and services
 		return render(c, Table(teams, services))
 	} else {
 		return errors.New("not enough services/teams provided")
 	}
 }
 
+// Render function to output the Templ component
 func render(ctx echo.Context, cmp templ.Component) error {
-	return cmp.Render(ctx.Request().Context(), ctx.Response())
+	return cmp.Render(context.Background(), ctx.Response())
 }
 
-// WebSocket handler
+// WebSocket handler to manage client connections
 func WebSocketHandler(c echo.Context) error {
 	ws, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 	if err != nil {
@@ -65,12 +71,13 @@ func BroadcastUpdates() {
 		time.Sleep(1 * time.Second)
 
 		mu.Lock()
-		// getting data from the score holder
+		// Get the updated data from score_holder
 		data := score_holder.GetMap()
 		mu.Unlock()
 
 		message, _ := json.Marshal(data)
 
+		// Send the update message to all WebSocket clients
 		for client := range clients {
 			err := client.WriteMessage(websocket.TextMessage, message)
 			if err != nil {

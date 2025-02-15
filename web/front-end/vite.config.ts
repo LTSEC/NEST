0bbaf44300c0675a1c8d27c11b7d6d2c83076f51
Kameron Stuckey <kstuckey817@gmail.com>
2025-02-14 23:48:59 -0600

    ServiceUptime Implementation
    - ServiceUptime is 90% of ServiceStatus.tsx code but with the percent score
    
    - Boxes show different color based on % (dark red, red, orange, green) like in the reference pictures
    
    - Graph is implemented in ShowGraphs.tsx in the "container" div
    
    - Updated vite.config.ts to add "manual chunks" to automatically split up large filess;
    
    - Also removed Tailwindcss from vite.config.ts because it waws never implemented or used anyways

--- web/front-end/vite.config.ts
+++ web/front-end/vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(), 
-    tailwindcss()
-  ],
-})
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ['react', 'react-dom'],
+          mantine: ['@mantine/core'],
+        },
+      },
+    },
+  },
+  plugins: [react()],
+});
